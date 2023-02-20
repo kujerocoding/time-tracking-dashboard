73c9458593a364585e08ec78693f975baf4f1a71
herom <jerome.manset22@gmail.com>
2023-02-21 03:34:36 +0800

    add elements to the Activity component

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Activity = () => {
   return (
-    <div>
-      Activity
+    <div className='activity--container'>
+        <div className='activity--icon--container'>
+            <img src="./src/assets/images/icon-work.svg" alt="suitcase icon" />
+        </div>
+        <div>
+            <p>Work</p>
+            <p>32hrs</p>
+            <img src="./src/assets/images/icon-ellipsis.svg" alt="Ellipsis icon" />
+            <p>Last Week - 8hrs</p>
+        </div>
     </div>
   )
 }
