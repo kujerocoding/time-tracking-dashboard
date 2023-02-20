656cafbd001b2b8c5445566a69f53b71a6c756e9
herom <jerome.manset22@gmail.com>
2023-02-21 06:03:33 +0800

    add attribution and style it

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,9 @@ const activityElements = data.map((data, index) => <Activity
     <div className='app--container'>
       <User handleClick={showTimeframe}/>
       {activityElements}
+      <div className='attribution'>
+            Challenge by <a href="https://www.frontendmentor.io/challenges" target="_blank">Frontend Mentor.</a> Coded by <a href='https://github.com/kujerocoding' target="_blank">kujerocoding</a>
+      </div>
     </div>
   )
 }
