00f14ca7e3ac785f36f645d0340553e54fd325ab
herom <jerome.manset22@gmail.com>
2023-02-21 02:57:31 +0800

    add elements to the User component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Activity from './components/Activity'
+import User from './components/User'
 
 const App = () => {
   return (
     <div>
-      app
+      <User />
+      <Activity />
     </div>
   )
 }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const User = () => {
   return (
-    <div>
-      User
+    <div className='user--container'>
+      <div>
+        <img src="/src/assets/images/image-jeremy.png" alt="jeremy profile picture" />
+        <p>Report for <span>Jeremy Robson</span></p>
+      </div>
+      <ul className='periodical--lists'>
+        <li>Daily</li>
+        <li>Weekly</li>
+        <li>Monthly</li>
+      </ul>
     </div>
   )
 }
