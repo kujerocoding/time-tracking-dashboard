349ac71268528b005b91e01949121964879c6e99
herom <jerome.manset22@gmail.com>
2023-02-21 04:47:36 +0800

    add switch case statement returning the image file name based on the data titles then call the imgURL from the last section of the img source url

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 
 const Activity = (props) => {
+
+    let imgURL;
+    
+    switch(props.title){
+        case 'Work':
+            imgURL = 'icon-work.svg';
+            break;
+        case 'Play':
+            imgURL = 'icon-play.svg';
+            break;
+        case 'Study':
+            imgURL = 'icon-study.svg';
+            break;
+        case 'Exercise':
+            imgURL = 'icon-exercise.svg';
+            break;
+        case 'Social':
+            imgURL = 'icon-social.svg';
+            break;
+        case 'Self Care':
+            imgURL = 'icon-self-care.svg';
+        default:
+    }
+
   return (
     <div className='activity--container'>
         <div className='activity--icon--container'>
-            <img src="./src/assets/images/icon-work.svg" alt="suitcase icon" />
+            <img src={`./src/assets/images/${imgURL}`} alt="suitcase icon" />
         </div>
         <div className='activity--details--container'>
             <div>
