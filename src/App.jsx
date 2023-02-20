674dba8f49051e817dc9d094bb1e37e6bc7b34e0
herom <jerome.manset22@gmail.com>
2023-02-21 03:28:51 +0800

    import data from data.json

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import Activity from './components/Activity'
 import User from './components/User'
 import './App.css'
+import data from '/data.json'
 
 const App = () => {
+
+console.log(data)
+
   return (
     <div className='app--container'>
       <User />
