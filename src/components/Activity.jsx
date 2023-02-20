816944ad898ff19229a99ed235f77298e6764f92
herom <jerome.manset22@gmail.com>
2023-02-21 04:03:17 +0800

    add styles to the Activity component

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -6,11 +6,15 @@ const Activity = () => {
         <div className='activity--icon--container'>
             <img src="./src/assets/images/icon-work.svg" alt="suitcase icon" />
         </div>
-        <div>
-            <p>Work</p>
-            <p>32hrs</p>
-            <img src="./src/assets/images/icon-ellipsis.svg" alt="Ellipsis icon" />
-            <p>Last Week - 8hrs</p>
+        <div className='activity--details--container'>
+            <div>
+                <p className='activity--title'>Work</p>
+                <p className='activity--hour'>32hrs</p>
+            </div>
+            <div>
+                <img src="./src/assets/images/icon-ellipsis.svg" alt="Ellipsis icon" />
+                <p className='activity--previous--hour'>Last Week - 8hrs</p>
+            </div>
         </div>
     </div>
   )
