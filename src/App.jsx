2f1c2f609e2dff233aa7440b90e7192e2490d3a4
herom <jerome.manset22@gmail.com>
2023-02-21 05:21:20 +0800

    update props in activityElements variable

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,17 @@ import data from '/data.json'
 
 const App = () => {
 
-console.log(data)
+//console.log(data)
 
-const activityElements = data.map(data => <Activity 
+const activityElements = data.map((data, index) => <Activity 
+  key={index}
   title={data.title}
   dailyCurrent={data.timeframes.daily.current}
   dailyPrevious={data.timeframes.daily.previous}
+  weeklyCurrent={data.timeframes.weekly.current}
+  weeklyPrevious={data.timeframes.weekly.previous}
+  monthlyCurrent={data.timeframes.monthly.current}
+  montlyPrevious={data.timeframes.monthly.previous}
    />)
 
   return (
